671f80b84beedb2a678e64d884d912c70a1a4cd3
PJKnightR <p.j.reagan99@gmail.com>
2024-08-25 17:52:00 -0400

    Tweaks to ore spawning. Added Explosives, Rocket Control Unit, Rocket Part, Low Density Structure

--- src/main/java/net/fpr/factorocraft/item/ModItems.java
+++ src/main/java/net/fpr/factorocraft/item/ModItems.java
@@ -36,11 +36,19 @@ public class ModItems {
 
     public static final RegistryObject<Item> PLASTIC = ITEMS.register("plastic", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> EXPLOSIVES = ITEMS.register("explosives", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
     public static final RegistryObject<Item> ELECTRIC_MOTOR = ITEMS.register("electric_motor", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<Item> FUEL = ITEMS.register("fuel", () -> new FuelItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<Item> NUCLEAR_FUEL = ITEMS.register("nuclear_fuel", () -> new NuclearFuelItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> LOW_DENSITY_STRUCTURE = ITEMS.register("low_density_structure", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> ROCKET_CONTROL_UNIT = ITEMS.register("rocket_control_unit", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> ROCKET_PART = ITEMS.register("rocket_part", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
